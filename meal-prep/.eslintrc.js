49e366b420c3aa33253f8cb4c0a0eccc6d8d9941
Murillo Ivamoto <ivamoto.murillo@gmail.com>
2020-08-12 13:20:25 -0300

    Initial commit

---
+++ meal-prep/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/airbnb'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        indent: ['error', 4],
+        quotes: ['error', 'single'],
+        'comma-dangle': ['error', 'never'],
+        'max-len': ['off', 160]
+    }
+};
---
+++ meal-prep/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <span>
+        <home-hero></home-hero>
+        <home-details></home-details>
+    </span>
+</template>
+
+<script>
+import HomeHero from '@/components/HomeHero.vue';
+import HomeDetails from '@/components/HomeDetails.vue';
+
+export default {
+    name: 'Home',
+    components: {
+        HomeHero,
+        HomeDetails
+    }
+};
+</script>
